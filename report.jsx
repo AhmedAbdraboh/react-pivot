1a8feeb509073e08abea134049e5c06ba9d7fa4e
Ahmed Abdraboh <ahmed.elsayed.abdraboh@gmail.com>
2019-05-31 01:36:48 +0200

    feat: create report component (#1)

--- report.jsx
+++ report.jsx
@@ -4,8 +4,60 @@ var createReactClass = require('create-react-class')
 
 var rows = require('./data.json')
 
+var dimensions = [
+  { title: 'Date', value: 'date' },
+  { title: 'Host', value: 'host' }
+]
+
+var reduce = function (row, memo) {
+
+  if (row.type == 'impression') {
+    memo.impressions = (memo.impressions || 0) + 1
+  }
+
+  if (row.type == 'load') {
+    memo.loads = (memo.loads || 0) + 1
+  }
+
+  if (row.type == 'display') {
+    memo.displays = (memo.displays || 0) + 1
+  }
+  return memo
+}
+
+var calculations = [
+  {
+    title: 'Impressions', value: 'impressions',
+    template: function (val, row) { return val }
+  },
+  {
+    title: 'Loads', value: 'loads',
+    template: function (val, row) { return val }
+  },
+  {
+    title: 'Displays', value: 'displays',
+    template: function (val, row) { return val }
+  },
+
+  {
+    title: 'Load Rate',
+    value: function (memo) { return memo.loads / memo.impressions },
+    template: function (val, row) { return (val * 100).toFixed(1) + '%' },
+  },
+  {
+    title: 'Display Rate',
+    value: function (memo) { return memo.displays / memo.loads },
+    template: function (val, row) { return (val * 100).toFixed(1) + '%' },
+  }
+]
+
 module.exports = createReactClass({
-  render () {
-    return <div>Report</div>
+  render() {
+    return <ReactPivot rows={rows}
+      dimensions={dimensions}
+      reduce={reduce}
+      calculations={calculations}
+      activeDimensions={['Transaction Type']} />
+
   }
 })
